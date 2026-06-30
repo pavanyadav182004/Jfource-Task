5a3de3e9ca779912497d2f17be74d01c7aed6e1d
PavanYadav <pavanyadav182004@gmail.com>
2026-06-30 23:13:18 +0530

    four

--- Task/src/Component/Login Page/Login.jsx
+++ Task/src/Component/Login Page/Login.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Login.css";
 
 export default function Login() {
+
+    const navigate = useNavigate();
 
     const [user, setUser] = useState({
         email: "",
@@ -10,6 +13,7 @@ export default function Login() {
     });
 
     const handleChange = (e) => {
+
         setUser({
             ...user,
             [e.target.name]: e.target.value
@@ -17,6 +21,7 @@ export default function Login() {
     };
 
     const handleSubmit = async (e) => {
+
         e.preventDefault();
 
         try {
@@ -25,61 +30,124 @@ export default function Login() {
                 `http://localhost:8080/auth/login?email=${user.email}&password=${user.password}`
             );
 
-            alert(res.data);
+            console.log(res.data);
 
-            if (res.data.includes("Admin")) {
-                window.location.href = "/admin";
-            } else {
-                window.location.href = "/feedback";
+            // Admin Login
+            if (res.data === "Admin Login Successful") {
+
+                alert("Admin Login Successful");
+
+                navigate("/admin");
+            }
+
+            // User Login
+            else if (
+                res.data ===
+                "User Login Successful"
+            ) {
+
+                alert("User Login Successful");
+
+                navigate("/feedback");
+            }
+
+            // Wrong Password
+            else if (
+                res.data ===
+                "Invalid Password"
+            ) {
+
+                alert("Invalid Password");
+            }
+
+            // User Not Found
+            else if (
+                res.data ===
+                "User Not Found"
+            ) {
+
+                alert("User Not Found");
+            }
+
+            else {
+
+                alert(res.data);
             }
 
         } catch (error) {
+
             console.log(error);
+
             alert("Login Failed");
         }
     };
 
     return (
-        <div className="login">
+        <>
+            <div className="login">
 
-            <h1>Login</h1>
-
-            <form onSubmit={handleSubmit}>
-
-                <input
-                    className="inpu"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={user.email}
-                    onChange={handleChange}
-                    required
-                />
-
-                <br />
-
-                <input
-                    className="inpu"
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={user.password}
-                    onChange={handleChange}
-                    required
-                />
-
-                <br /><br />
-
-                <button className="btn" type="submit">
+                <h1>
                     Login
-                </button>
+                </h1>
 
-                <p>
-                    Don't have an account?
-                    <a href="/register"> Sign Up</a>
-                </p>
+                <form
+                    onSubmit={
+                        handleSubmit
+                    }
+                >
 
-            </form>
-        </div>
+                    <input
+                        className="inpu"
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={
+                            user.email
+                        }
+                        onChange={
+                            handleChange
+                        }
+                        required
+                    />
+
+                    <br />
+
+                    <input
+                        className="inpu"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={
+                            user.password
+                        }
+                        onChange={
+                            handleChange
+                        }
+                        required
+                    />
+
+                    <br />
+                    <br />
+
+                    <button
+                        className="btn"
+                        type="submit"
+                    >
+                        Login
+                    </button>
+
+                    <p>
+                        Don't have an account?
+
+                        <a href="/register">
+                            {" "}
+                            Sign Up
+                        </a>
+                    </p>
+
+                </form>
+
+            </div>
+        </>
     );
 }
